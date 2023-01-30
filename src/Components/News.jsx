c55ffa955f64111195ead599d923ba752e3c1431
Pratik <pratikkaple1nov2002@gmail.com>
2023-01-30 19:16:43 +0530

    Fetching done,introducing buttons but not working

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -3,68 +3,54 @@ import NewsItem from './NewsItem'
 
 export default class News extends Component {
 
-    articles = [
-        {
-            "source": {
-                "id": null,
-                "name": "Moneycontrol"
-            },
-            "author": "TK Arun",
-            "title": "Budget 2023: Build new cities, it is what our future demands of the present",
-            "description": "By 2051, India may have an additional 335 million urban population. Several new cities will be needed to settle them",
-            "url": "https://www.moneycontrol.com/news/business/economy/budget-2023-build-new-cities-it-is-what-our-future-demands-of-the-present-9952651.html",
-            "urlToImage": "https://images.moneycontrol.com/static-mcnews/2023/01/budget-real-estate4-770x433.jpg",
-            "publishedAt": "2023-01-28T03:52:28Z",
-            "content": "India needs more than a pat on the back from fiscal-deficit-focused rating agencies and analysts, in order to regain economic vigour in a slowing world. A whole lot more. India needs a new New Deal, … [+5355 chars]"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "Iasbaba.com"
-            },
-            "author": "IASbaba",
-            "title": "Miniature votive stupas at Nalanda",
-            "description": "Context: Archeological Survey of India (ASI) has recently discovered two 1200-year-old miniature votive stupas during landscaping activities near Sarai Tila mound on the premises of ‘Nalanda Mahavihara’, a world heritage site in Nalanda district. About Miniat…",
-            "url": "https://iasbaba.com/2023/01/miniature-votive-stupas-at-nalanda/",
-            "urlToImage": "https://iasbaba.com/wp-content/uploads/2023/01/nm.png",
-            "publishedAt": "2023-01-17T13:25:06Z",
-            "content": "Context: Archeological Survey of India (ASI) has recently discovered two 1200-year-old miniature votive stupas during landscaping activities near Sarai Tila mound on the premises of Nalanda Mahavihar… [+2202 chars]"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "Thefreedictionary.com"
-            },
-            "author": null,
-            "title": "The British Museum Opens (1759)",
-            "description": "When the British Museum opened to the public in 1759, its exhibits were based largely on personal collections, including Sir Hans Sloane's Cabinet of Curiosities, Robert Harley's library, and Sir Robert Cotton's antiquities. Today, the museum is home to more …",
-            "url": "https://encyclopedia.thefreedictionary.com/British+Museum",
-            "urlToImage": "http://img.tfd.com/TFDlogo1200x1200.png",
-            "publishedAt": "2023-01-15T05:00:00Z",
-            "content": "The British Museum is a museum in London dedicated to human history and culture. Its permanent collection, numbering some 8 million works,[3] is among the largest and most comprehensive in existence[… [+66041 chars]"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "NDTV News"
-            },
-            "author": null,
-            "title": "Andhra To Investigate Stampede At Chandrababu Naidu Event",
-            "description": "The Andhra Pradesh government late on Saturday night constituted a one-man Commission of Inquiry to probe into the circumstances that led to two incidents of stampede in which 11 persons were killed in recent days.",
-            "url": "https://www.ndtv.com/india-news/andhra-pradesh-appoints-inquiry-commission-to-investigate-recent-stampedes-3672903",
-            "urlToImage": "https://c.ndtvimg.com/2023-01/u0ovta3o_chandrababu-naidu-stampede_650x400_01_January_23.jpg",
-            "publishedAt": "2023-01-07T17:30:39Z",
-            "content": "The recent two stampedes in the state killed a total of 11 people. (File) Amaravati: The Andhra Pradesh government late on Saturday night constituted a one-man Commission of Inquiry to probe into th… [+1451 chars]"
-        },
-    ];
-
+    articles = [];
 
     constructor() {
         super();
         this.state = {
             articles: this.articles,
-            loading: false
+            loading: false,
+            page: 1,
         }
+    }
+
+    async componentDidMount() {
+        let url = `https://newsapi.org/v2/everything?q=mumbai&from=2022-12-30&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}&pagesize=12`;
+        let data = await fetch(url)
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles });
+    };
+
+
+    async handlePrevClick() {
+
+        let url = `https://newsapi.org/v2/everything?q=mumbai&from=2022-12-30&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}&page=${this.state.page - 1}&pagesize=12`;
+        let data = await fetch(url)
+        let parsedData = await data.json();
+        // console.log(parsedData);
+        this.setState(
+            {
+                page: this.state.page - 1,
+                articles: parsedData.articles
+            }
+        )
+    }
+
+
+    async handleNextClick() {
+
+        console.log("Next cliked");
+        let url = `https://newsapi.org/v2/everything?q=mumbai&from=2022-12-30&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}&pagesize=12&page=${this.state.page + 1}&pagesize=12`;
+        let data = await fetch(url)
+        let parsedData = await data.json();
+        // console.log(parsedData);
+        this.setState(
+            {
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            }
+        )
     }
 
     render() {
@@ -74,12 +60,17 @@ export default class News extends Component {
                 <div className="row my-3">
                     {this.state.articles.map((element) => {
                         return (<div className="col-md-auto  my-2" key={element.url}>
-                            <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} newsURL={element.url} imageURL={element.urlToImage} />
+                            <NewsItem title={element.title} description={element.description} newsURL={element.url} imageURL={element.urlToImage} />
                         </div>);
                     })}
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.handlePrevCLick}>&laquo; Previous</button>
+                    <button className='btn btn-dark' onClick={this.handleNextClick}>Next &raquo;</button>
                 </div>
             </div >
 
         )
     }
 }
+
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -9,11 +9,11 @@ export default class NewsItem extends Component {
         return (
             <div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageURL} className="card-img-top" alt="..." />
+                    <img src={imageURL} className="card-img-top" alt="..." style={{ height: "100%" }} />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsURL} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                        <a rel="noreferrer" href={newsURL} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
@@ -23,5 +23,3 @@ export default class NewsItem extends Component {
     }
 }
 
-// API KEY - 29d910e6039445aab5463ebbe81c18eb
-// GET - "https://newsapi.org/v2/everything?q=tesla&from=2022-12-30&sortBy=publishedAt&apiKey=29d910e6039445aab5463ebbe81c18eb"
