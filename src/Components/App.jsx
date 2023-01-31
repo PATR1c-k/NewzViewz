e0a8d2274ba55ded1070438761f787f41ccbbd45
Pratik <pratikkaple1nov2002@gmail.com>
2023-02-01 02:04:05 +0530

    All the NAvbar functionality working

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -12,15 +12,17 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <Navbar />
+
                 <Router>
+                    <Navbar />
                     <Routes>
-                        <Route exact path="/home" element={<News pagesize="6" country="in" category="general" />} />
-                        <Route exact path="/business" element={<News pagesize="6" country="in" category="business" />} />
-                        <Route exact path="/entertainment" element={<News pagesize="6" country="in" category="entertainment" />} />
-                        <Route exact path="/healthscience" element={<News pagesize="6" country="in" category="healthscience" />} />
-                        <Route exact path="/sports" element={<News pagesize="6" country="in" category="sports" />} />
-                        <Route exact path="/technology" element={<News pagesize="6" country="in" category="technology" />} />
+                        <Route exact path="/" element={<News pagesize="8" country="in" category="general" />} />
+                        <Route exact path="/business" element={<News pagesize="8" country="in" category="business" />} />
+                        <Route exact path="/entertainment" element={<News pagesize="8" country="in" category="entertainment" />} />
+                        <Route exact path="/health" element={<News pagesize="8" country="in" category="health" />} />
+                        <Route exact path="/science" element={<News pagesize="8" country="in" category="science" />} />
+                        <Route exact path="/sports" element={<News pagesize="8" country="in" category="sports" />} />
+                        <Route exact path="/technology" element={<News pagesize="8" country="in" category="technology" />} />
                     </Routes>
                 </Router>
                 {/* <h1>Hello Class based component {this.c}</h1> */}
